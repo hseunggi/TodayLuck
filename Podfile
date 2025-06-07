fdd81c2cdb55c6a17f6d33006d0b356ff28977ee
hseunggi <hong922a@naver.com>
2025-06-06 18:14:07 -0700

    Feat:firebase연동

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '12.0'
+
+target 'TodayLuck' do
+  use_frameworks!
+
+  pod 'Firebase/Core', '8.15.0'         # Firebase 8.x 버전은 Xcode 12.5와 호환 가능
+  pod 'Firebase/Firestore', '8.15.0'
+end
+
--- TodayLuck/HomeController.swift
+++ TodayLuck/HomeController.swift
@@ -1,14 +1,27 @@
 
 import UIKit
+import FirebaseFirestore
 
 class HomeController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let db = Firestore.firestore()
+//                db.collection("test").addDocument(data: [
+//                    "name": "홍길동",
+//                    "created": Timestamp(date: Date())
+//                ]) { err in
+//                    if let err = err {
+//                        print("❌ 에러 발생: \(err)")
+//                    } else {
+//                        print("✅ Firestore에 데이터 저장 성공!")
+//                    }
+//                }
         // Do any additional setup after loading the view.
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(tap)
+        
     }	
     @objc func dismissKeyboard() {
         view.endEditing(true)
